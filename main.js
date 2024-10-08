0a3e4488c5144238c84f476a754da34089a7fe60
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-10-08 15:32:40 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+document.querySelector('#apple-logo').addEventListener('click', function(e){
+    window.open('https://www.apple.com');
+})
+
+document.querySelector('.iphone-16-pro img').addEventListener('click', function(e){
+    window.open('https://www.apple.com/iphone-16-pro/');
+})
+
+document.querySelector('#learn-more-btn').addEventListener('click', function(e){
+    window.open('https://www.apple.com/iphone-16-pro/');
+})
+
+document.querySelector('#buy-btn').addEventListener('click', function(e){
+    window.open('https://www.apple.com/shop/buy-iphone/iphone-16-pro');
+})
